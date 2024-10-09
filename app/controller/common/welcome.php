cc15cfe87e0f27ab0ea6b669c771f108649d8612
Andrii Riabchenko <ryabchenko.andrew@gmail.com>
2024-10-09 20:00:38 +0300

    Let's clean up the garbage

--- app/controller/common/welcome.php
+++ app/controller/common/welcome.php
@@ -2,8 +2,6 @@
 
 class ControllerCommonWelcome extends Controller {
     public function index() {
-        $this->app->useModel('user/user');
-
         $this->data['cover'] = $this->staticfile->getUri('img/welcome.png');
 
         return $this->view->template('common/welcome', $this->data);
